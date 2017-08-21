2b94d096defc46f32fc8e7d42a8c6bbc37a38136
Ankush Malik <ankushmalik631@gmail.com>
2017-08-21 19:21:05 +0530

    homepage tweaked a little

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+#Formatting DateTime to look like "20/01/2011 10:28PM"
+Time::DATE_FORMATS[:default] = "%d/%m/%Y %l:%M%p"
